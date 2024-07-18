c70ebb640b27e38570b93b2b14b949f894180995
phpablo <pablohenrique120@gmail.com>
2024-07-18 11:56:25 -0300

    :bugfix: Fix DB Info
    Fixed DB info
    Remove Controller do not use

--- src/Config.php
+++ src/Config.php
@@ -3,11 +3,11 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = '/mod9_MVC/public';
+    const BASE_DIR = '/Projeto005_Devsbook/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'test';
+    const DB_DATABASE = 'devsbook';
     const DB_USER = 'root';
     const DB_PASS = '';
 
