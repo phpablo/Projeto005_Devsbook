e1f8bcc35205876cc9f26632fbf2227aedd22f71
phpablo <pablohenrique120@gmail.com>
2024-07-24 14:53:27 -0300

    Novo  title

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,10 +1,5 @@
 <?php $render('header'); ?>
 
-<?php echo 'Eu sou ' . $profissao . ' Meu nome é ' . $nome . ' e tenho ' . $idade . ' anos.' ?>
+<?php echo ' Meu nome é ' . $nome ?>
 
 <hr>
-
-<?php foreach ($posts as $post) : ?>
-  <h3><?php echo $post['Titulo']; ?></h3>
-  <p><?php echo $post['Corpo']; ?></p>
-<?php endforeach; ?>
