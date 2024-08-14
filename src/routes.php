72bb8cc02842bf755983b05fbcba301e6f9d586f
phpablo <pablohenrique120@gmail.com>
2024-08-14 11:58:52 -0300

    :fire: Remove useless comments
    Removed unnecessary comments to improve code readability

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,4 @@
 <?php
-/*
-:sparkles: Add routes and functions for user registration
-
-- Added routes and functions to handle user registration
-- Implemented verification logic to check if a user already exists
-- Ensured consistent pattern for user existence verification
-
-*/
 
 use core\Router;
 
