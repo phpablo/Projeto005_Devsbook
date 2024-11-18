568b94732a1fbe83b0af84bafe33c47b9ecd17a8
phpablo <pablohenrique120@gmail.com>
2024-11-18 09:25:46 -0300

    :sparkles: Added logout function
    - Add function to logout

--- src/routes.php
+++ src/routes.php
@@ -17,6 +17,8 @@ $router->post('/post/new', 'PostController@new');
 $router->get('/perfil/{id}', 'ProfileController@index');
 $router->get('/perfil', 'ProfileController@index');
 
+$router->get('/sair', 'LoginController@logout');
+
 // $router->get('/pesquisa');
 // $router->get('/perfil');
 // $router->get('/sair');
